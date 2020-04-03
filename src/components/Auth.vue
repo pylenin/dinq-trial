08ef97693b5ba6832e7da91e16b628acfae3d301
Lenin Mishra <lenin.mishra@eleena.nl>
2020-04-03 22:57:48 +0200

    Add an html form

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -16,8 +16,7 @@
                 fill-height
                 justify-center
                 align-center
-                v-if="loading"
-              >
+                v-if="loading">
                 <v-progress-circular
                   :size="50"
                   color="primary"
@@ -25,7 +24,29 @@
                 />
               </v-layout>
 
+<form method="post">
+  <div class="imgcontainer">
+    <img src="img_avatar2.png" alt="Avatar" class="avatar">
+  </div>
 
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
               <v-form v-else ref="form" v-model="valid" lazy-validation>
                 <v-container>
 
