dadc858ead2b251955cc2600cf5186b0eeaef323
Lenin Mishra <lenin.mishra@eleena.nl>
2020-04-03 23:00:37 +0200

    Remove vue specifi code from auth page

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,28 +1,5 @@
 <template>
-    <v-container grid-list-md>
-      <v-layout row wrap align-center justify-center fill-height>
-        <v-flex xs12 sm8 lg4 md5>
-          <v-card class="login-card">
-            <v-card-title>
-              <span class="headline">Login to Dinq</span>
-            </v-card-title>
-
-            <v-spacer/>
-
-            <v-card-text>
-
-              <v-layout
-                row
-                fill-height
-                justify-center
-                align-center
-                v-if="loading">
-                <v-progress-circular
-                  :size="50"
-                  color="primary"
-                  indeterminate
-                />
-              </v-layout>
+<h1>Login to Dinq</h1>
 
 <form method="post">
   <div class="imgcontainer">
@@ -47,37 +24,8 @@
     <span class="psw">Forgot <a href="#">password?</a></span>
   </div>
 </form>
-              <v-form v-else ref="form" v-model="valid" lazy-validation>
-                <v-container>
+              
 
-                  <v-text-field
-                    v-model="credentials.username"
-                    :counter="70"
-                    label="email address"
-                    maxlength="70"
-                    required
-                  />
-
-                  <v-text-field
-                    type="password"
-                    v-model="credentials.password"
-                    :counter="20"
-                    label="password"
-                    maxlength="20"
-                    required
-                  />
-
-                </v-container>
-                <v-btn class="pink white--text" :disabled="!valid" @click="login">Login</v-btn>
-
-              </v-form>
-
-
-            </v-card-text>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
 </template>
 
 <script>
